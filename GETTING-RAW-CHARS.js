2097fa832e53a5d5fff5245029d3bc22625faab7
Daniel H <dh.danielhister@gmail.com>
2020-02-01 23:42:56 +0100

    Update GETTING-RAW-CHARS.js

--- GETTING-RAW-CHARS.js
+++ GETTING-RAW-CHARS.js
@@ -1,9 +1,9 @@
-// Use for getting chars on your keyboard (including backspace, ctrl and so on...)
+// Use for getting chars on your keyboard (including backspace, ctrl and so on...) If the char is not existing in unicode, it will escape the char.
 
-let stdin = process.stdin;
-stdin.setEncoding("utf8");
-stdin.setRawMode(true);
+let stdin = process.stdin; // Aliasing proces.stdin.
+stdin.setEncoding("utf8"); // You don't want binary buffers - Do you?
+stdin.setRawMode(true); // Backspace and so on are getting read too.
 
-stdin.on("data", chunk => {
-  console.log(JSON.stringify(chunk))
+stdin.on("data", chunk => { // Called when any key is pressed.
+  console.log(JSON.stringify(chunk)) // Logging an escaped version of the typed char.
 });
