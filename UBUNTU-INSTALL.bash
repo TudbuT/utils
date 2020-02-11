cd2ff59c801a359b08d7b1c9aabb4a9f54dd7442
Daniel H <dh.danielhister@gmail.com>
2020-02-11 15:12:47 +0000

    Create UBUNTU-INSTALL.bash

---
+++ UBUNTU-INSTALL.bash
@@ -0,0 +1,67 @@
+folder=ubuntu-fs
+if [ -d "$folder" ]; then
+    first=1
+    echo "skipping downloading"
+fi
+tarball="ubuntu.tar.gz"
+if [ "$first" != 1 ];then
+    if [ ! -f $tarball ]; then
+	echo "downloading ubuntu-image"
+	case `dpkg --print-architecture` in
+	aarch64)
+	    archurl="arm64" ;;
+	arm)
+	    archurl="armhf" ;;
+	armhf)
+	    archurl="armhf" ;;
+	amd64)
+	    archurl="amd64" ;;
+	i*86)
+	    archurl="i386" ;;
+	x86_64)
+	    archurl="amd64" ;;
+	*)
+	    echo "unknown architecture"; exit 1 ;;
+	esac
+	wget "https://partner-images.canonical.com/core/disco/current/ubuntu-disco-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
+    fi
+    cur=`pwd`
+    mkdir -p "$folder"
+    cd "$folder"
+    echo "decompressing ubuntu image"
+    tar -xf ${cur}/${tarball} --exclude='dev'||:
+    echo "fixing nameserver, otherwise it can't connect to the internet"
+    echo "nameserver 1.1.1.1" > etc/resolv.conf
+    cd "$cur"
+fi
+mkdir -p binds
+echo "setting up"
+bin=start-ubuntu.sh
+sudo chmod 777 ubuntu-fs
+sudo chroot ./ubuntu-fs /bin/apt-get clean
+sudo chroot ./ubuntu-fs /bin/mv /var/lib/apt/lists /var/lib/apt/lists.old
+sudo chroot ./ubuntu-fs /bin/mkdir -p /var/lib/apt/lists/partial
+sudo chroot ./ubuntu-fs /bin/apt-get clean
+sudo chroot ./ubuntu-fs /bin/apt-get update
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+command="sudo chroot ./ubuntu-fs /bin/bash /boot.sh"
+fl=" HOME=/root"
+fl+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+fl+=" TERM=\$TERM"
+fl+=" LANG=C.UTF-8"
+fl+=" /bin/bash --login"
+echo "\$fl" > ubuntu-fs/boot.sh
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+echo "making $bin executable"
+chmod +x $bin
+echo "You can now launch Ubuntu with the ./${bin} script"
